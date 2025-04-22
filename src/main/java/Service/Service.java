73c891a49f5a6a9e47aad9c2e6a8e85f473d49d2
Santiadi <disacamo43@gmail.com>
2025-04-22 11:31:44 -0500

    variblaes de entorno

--- src/main/java/Service/Service.java
+++ src/main/java/Service/Service.java
@@ -4,16 +4,28 @@ import controller.implementations.FileDepotImplementation;
 import grpc.GrpcNodeManager;
 import jakarta.xml.ws.Endpoint;
 import rmi.AuthService;
+import util.ConfigLoader;
 
 public class Service {
     public static void main(String[] args) {
         try {
-            AuthService.init("localhost", 4423, "AuthService");
+            //RMI
+            String rmiHost = ConfigLoader.get("RMI_HOST");
+            int rmiPort = ConfigLoader.getInt("RMI_PORT");
+            String rmiServiceName = ConfigLoader.get("RMI_SERVICE_NAME");
+            AuthService.init(rmiHost, rmiPort, rmiServiceName);
 
+            //gRPC
             GrpcNodeManager.verifyConnectionReady();
 
-            Endpoint.publish("http://localhost:2005/FileDepotService", new FileDepotImplementation());
-            System.out.println("Servicio SOAP iniciado en http://localhost:2005/FileDepotService?wsdl");
+            //SOAP
+            int soapPort = ConfigLoader.getInt("SOAP_PORT");
+            String soapPath = ConfigLoader.get("SOAP_BASE_PATH");
+            String soapHost = ConfigLoader.get("SOAP_HOST");
+            String soapURL = "http://" + soapHost + ":" + soapPort + soapPath;
+
+            Endpoint.publish(soapURL, new FileDepotImplementation());
+            System.out.println(soapURL);
 
         } catch (Exception e) {
             System.err.println("Error al iniciar el sistema: " + e.getMessage());
